812f6a473c82239127d535a76f88ffd89ddbf7ca
ismaelpereira <ismael@bankuish.com>
2025-05-27 19:02:25 -0300

    removing names that are not subscribing and adding new subscribers

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -8,9 +8,29 @@ export type Player = {
 
 export const PLAYER_LIST: Player[] = [
    {
+      image: "player_images/Henrique.png",
+      name: "Henrique",
+      position: "Atacante",
+   },
+   {
+      image: "player_images/Lent.png",
+      name: "Lent",
+      position: "Atacante",
+   },
+   {
+      image: "player_images/Victor_R.png",
+      name: "Victor R",
+      position: "Atacante",
+   },
+   {
+      image: "player_images/Vinicius.png",
+      name: "Vinícius",
+      position: "Atacante",
+   },
+   {
       image: "player_images/Almeida.png",
       name: "Almeida",
-      position: "Meia",
+      position: "Atacante",
    },
    {
       image: "player_images/Bellei.png",
@@ -18,9 +38,39 @@ export const PLAYER_LIST: Player[] = [
       position: "Atacante",
    },
    {
-      image: "player_images/Esteban.png",
-      name: "Esteban",
-      position: "Defensor",
+      image: "player_images/Champz.png",
+      name: "Champz",
+      position: "Atacante",
+   },
+   {
+      image: "player_images/Enrico.png",
+      name: "Enrico",
+      position: "Atacante",
+   },
+   {
+      image: "player_images/Patrick.png",
+      name: "Patrick",
+      position: "Atacante",
+   },
+   {
+      image: "player_images/Mayller.png",
+      name: "Mayller",
+      position: "Atacante",
+   },
+   {
+      image: "player_images/Sartori.png",
+      name: "Sartori",
+      position: "Atacante",
+   },
+   {
+      image: "player_images/Yan.png",
+      name: "Yan",
+      position: "Atacante",
+   },
+   {
+      image: "player_images/Daniel_A.png",
+      name: "Daniel A",
+      position: "Meia",
    },
    {
       image: "player_images/Felipe_Bignoto.png",
@@ -33,63 +83,28 @@ export const PLAYER_LIST: Player[] = [
       position: "Meia",
    },
    {
-      image: "player_images/Marcelino.png",
-      name: "Marcelino",
+      image: "player_images/Lucas_T.png",
+      name: "Lucas T",
       position: "Meia",
    },
    {
-      image: "player_images/Nick.png",
-      name: "Nick",
-      position: "Defensor",
+      image: "player_images/anonymous.png",
+      name: "Hugo A",
+      position: "Meia",
    },
    {
-      image: "player_images/Nilvo.png",
-      name: "Nilvo",
-      position: "Goleiro",
+      image: "player_images/Coelho.png",
+      name: "Coelho",
+      position: "Meia",
    },
    {
-      image: "player_images/Otavio.png",
-      name: "Otavio",
-      position: "Defensor",
+      image: "player_images/davi_b.png",
+      name: "Davi B",
+      position: "Meia",
    },
    {
-      image: "player_images/Patrick.png",
-      name: "Patrick",
-      position: "Atacante",
-   },
-   {
-      image: "player_images/Pazos.png",
-      name: "Pazos",
-      position: "Defensor",
-   },
-   {
-      image: "player_images/Pedro_Henrique.png",
-      name: "Pedro Henrique",
-      position: "Defensor",
-   },
-   {
-      image: "player_images/PP.png",
-      name: "PP",
-      position: "Goleiro",
-   },
-   {
-      image: "player_images/Samuka.png",
-      name: "Samuka",
-      position: "Goleiro",
-   },
-   {
-      image: "player_images/Predo.png",
-      name: "Predo",
-      position: "Defensor",
-   },
-   {
-      image: "player_images/Romulo_S.png",
-      name: "Rômulo S",
-      position: "Defensor",
-   },
-   {
-      image: "player_images/LG.png",
-      name: "LG",
+      image: "player_images/Davi_C.png",
+      name: "Davi C",
       position: "Meia",
    },
    {
@@ -98,18 +113,33 @@ export const PLAYER_LIST: Player[] = [
       position: "Meia",
    },
    {
-      image: "player_images/Justin.png",
-      name: "Justin",
-      position: "Atacante",
+      image: "player_images/LG.png",
+      name: "LG",
+      position: "Meia",
    },
    {
-      image: "player_images/Sartori.png",
-      name: "Sartori",
-      position: "Atacante",
+      image: "player_images/Marcelino.png",
+      name: "Marcelino",
+      position: "Meia",
    },
    {
-      image: "player_images/Sergio.png",
-      name: "Sergio",
+      image: "player_images/Predo.png",
+      name: "Predo",
+      position: "Defensor",
+   },
+   {
+      image: "player_images/Brunin.png",
+      name: "Brunin",
+      position: "Defensor",
+   },
+   {
+      image: "player_images/Esteban.png",
+      name: "Esteban",
+      position: "Defensor",
+   },
+   {
+      image: "player_images/Pazos.png",
+      name: "Pazos",
       position: "Defensor",
    },
    {
@@ -118,43 +148,8 @@ export const PLAYER_LIST: Player[] = [
       position: "Defensor",
    },
    {
-      image: "player_images/Lent.png",
-      name: "Lent",
-      position: "Atacante",
-   },
-   {
-      image: "player_images/Henrique.png",
-      name: "Henrique",
-      position: "Atacante",
-   },
-   {
-      image: "player_images/Henrique_Carius.png",
-      name: "Henrique Cariús",
-      position: "Atacante",
-   },
-   {
-      image: "player_images/Enrico.png",
-      name: "Enrico",
-      position: "Atacante",
-   },
-   {
       image: "player_images/Bernardo.png",
       name: "Bernardo",
-      position: "Defensor",
-   },
-   {
-      image: "player_images/Coelho.png",
-      name: "Coelho",
-      position: "Meia",
-   },
-   {
-      image: "player_images/Digão.png",
-      name: "Digão",
-      position: "Defensor",
-   },
-   {
-      image: "player_images/Davi_Valle.png",
-      name: "Davi Valle",
       position: "Defensor",
    },
    {
@@ -163,9 +158,34 @@ export const PLAYER_LIST: Player[] = [
       position: "Defensor",
    },
    {
-      image: "player_images/Daniel_A.png",
-      name: "Daniel A",
-      position: "Meia",
+      image: "player_images/Davi_Valle.png",
+      name: "Davi Valle",
+      position: "Defensor",
+   },
+   {
+      image: "player_images/Osmir.png",
+      name: "Osmir",
+      position: "Defensor",
+   },
+   {
+      image: "player_images/Nick.png",
+      name: "Nick",
+      position: "Defensor",
+   },
+   {
+      image: "player_images/Pedro_Henrique.png",
+      name: "Pedro Henrique",
+      position: "Defensor",
+   },
+   {
+      image: "player_images/Romulo_S.png",
+      name: "Rômulo S",
+      position: "Defensor",
+   },
+   {
+      image: "player_images/Sergio.png",
+      name: "Sergio",
+      position: "Defensor",
    },
    {
       image: "player_images/Borges.png",
@@ -173,45 +193,30 @@ export const PLAYER_LIST: Player[] = [
       position: "Goleiro",
    },
    {
-      image: "player_images/Brunin.png",
-      name: "Brunin",
-      position: "Defensor",
-   },
-   {
       image: "player_images/Carlos.png",
       name: "Carlos",
       position: "Goleiro",
    },
    {
-      image: "player_images/davi_b.png",
-      name: "Davi B",
-      position: "Meia",
-   },
-   {
-      image: "player_images/Vinicius.png",
-      name: "Vinícius",
-      position: "Atacante",
-   },
-   {
-      image: "player_images/Yan.png",
-      name: "Yan",
-      position: "Atacante",
-   },
-   {
-      image: "player_images/Victor_R.png",
-      name: "Victor R",
-      position: "Atacante",
-   },
-   {
-      image: "player_images/Champz.png",
-      name: "Champz",
-      position: "Atacante",
-   },
-   {
-      image: "player_images/Davi_C.png",
-      name: "Davi C",
+      image: "player_images/Mudinho.png",
+      name: "Mudinho",
       position: "Goleiro",
    },
+   {
+      image: "player_images/Nilvo.png",
+      name: "Nilvo",
+      position: "Goleiro",
+   },
+   {
+      image: "player_images/Samuka.png",
+      name: "Samuka",
+      position: "Goleiro",
+   },
+   {
+      image: "player_images/Pedro_Assis.png",
+      name: "Pedro Assis",
+      position: "Goleiro",
+   }
 ];
 
 export const ItemTypes = {
